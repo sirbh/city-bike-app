cc009f34f4b9b70e61c4f56c06351fa204805df0
saurabh chauhan <sourabh1202@gmail.com>
2022-12-29 23:37:30 +0200

    removed unwanted code

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,9 +1,6 @@
-import { PedalBike, Menu as MenuIcon } from '@mui/icons-material';
+import { PedalBike } from '@mui/icons-material';
 import Box from '@mui/material/Box';
 import { AppBar, Container, Toolbar, Typography } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
@@ -36,49 +33,6 @@ function Navbar() {
               CityBike
             </Typography>
           </Link>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              //   onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              //   anchorEl={anchorElNav}
-              //   anchorOrigin={{
-              //     vertical: 'bottom',
-              //     horizontal: 'left',
-              //   }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open
-              //   onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <Link
-                  to={page.route}
-                  key={page.name}
-                  style={{ textDecoration: 'none' }}
-                >
-                  <MenuItem>
-                    <Typography textAlign="center">{page.name}</Typography>
-                  </MenuItem>
-                </Link>
-              ))}
-            </Menu>
-          </Box>
           <Typography
             variant="h5"
             noWrap
