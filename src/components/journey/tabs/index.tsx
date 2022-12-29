47a5c38448f75be2087859b8d0a459e351e51d25
saurabh chauhan <sourabh1202@gmail.com>
2022-12-29 12:22:10 +0200

    fixed tabs

--- src/components/journey/tabs/index.tsx
+++ src/components/journey/tabs/index.tsx
@@ -2,28 +2,63 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { TakeoutDining } from '@mui/icons-material';
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
+import { Button, Typography } from '@mui/material';
 
-export default function CenteredTabs() {
-  const [value, setValue] = React.useState(0);
+interface ITabs {
+  tabsDetails: {
+    name: string;
+    value: string;
+    order: string;
+  }[];
+  seletedTab: number;
+  onTabClick: (newValue: number) => void;
+}
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+function getLabel(order: string) {
+  if (order === 'asc') {
+    return <ArrowDownward />;
+  }
 
+  return <ArrowUpward />;
+}
+
+export default function CenteredTabs({
+  tabsDetails,
+  seletedTab,
+  onTabClick,
+}: ITabs) {
   return (
     <Box
-      sx={{ width: '100%', bgcolor: 'background.paper', marginBottom: '4rem' }}
+      sx={{
+        width: '100%',
+        bgcolor: 'background.paper',
+        margin: '2rem 0rem',
+        display: 'flex',
+        alignItems: 'center',
+      }}
     >
-      <Tabs value={value} onChange={handleChange}>
-        <Tab label="Sort By:" disabled />
-        <Tab
-          label="Item Two"
-          icon={<TakeoutDining />}
-          sx={{ display: 'flex' }}
-        />
-        <Tab label="Item Three" />
-      </Tabs>
+      <Typography
+        variant="h6"
+        sx={{ display: 'inline-block', margin: '0rem 1rem' }}
+      >
+        Sort By:
+      </Typography>
+      {tabsDetails.map((tab, i) => {
+        return (
+          <Button
+            key={tab.value}
+            onClick={() => {
+              onTabClick(i);
+            }}
+            endIcon={seletedTab === i ? getLabel(tab.order) : undefined}
+            sx={{ marginLeft: '1rem' }}
+            variant={seletedTab === i ? 'contained' : 'outlined'}
+          >
+            {tab.name}
+          </Button>
+        );
+      })}
     </Box>
   );
 }
